97c52b46a3ae176a0d2b17d619e18ad7706f4eb7
Jonas Forsberg <himynameisjonas@gmail.com>
2011-01-29 20:27:57 +0100

    init!!!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flickrblog::Application.config.secret_token = 'c66dab844abda807b7fb2281ed81797fa31a52e24cdf618e552152b87da2117476af757fe410a164e8e42e02c6d11661016c870733b5eeed9065a2887682d546'
