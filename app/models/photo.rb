16970e7494b68d3df2c553980789de87a7625775
Jonas Forsberg <himynameisjonas@gmail.com>
2011-01-29 23:50:28 +0100

    added next and previous for photos

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,5 @@
 class Photo
-  attr_accessor :id, :thumbnail_url, :large_url
+  attr_accessor :id, :thumbnail_url, :large_url, :index
   require 'open-uri'
   
   def initialize(attributes = {})
@@ -10,17 +10,29 @@ class Photo
   
   def self.all
     doc = Nokogiri::XML(open("http://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=c794c3f00be2130085e3cdef2f06aeb6&photoset_id=72157625847846478&extras=url_sq%2Curl_m%2Curl_l%2Curl_o"))
-    doc.css("photo").map do |photo|
-      self.new(
+    photos = Array.new
+    doc.css("photo").to_a.each_index do |index|
+      photo = doc.css("photo")[index]
+      photos[index] = self.new(
         :id => photo.attr("id"),
         :thumbnail_url => photo.attr("url_sq"),
-        :large_url => photo.attr("url_l")
+        :large_url => photo.attr("url_l"),
+        :index => index
       )
     end
+    photos
   end
   
   def self.find(id)
     self.all.select{|photo| photo.id == id}.first
+  end
+  
+  def next
+    self.class.all.fetch(self.index + 1, nil)
+  end
+  
+  def previous
+    self.class.all[self.index - 1] unless self.index-1 < 0
   end
   
   def to_param
