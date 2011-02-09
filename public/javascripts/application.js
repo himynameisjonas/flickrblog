ce012bc7d20a58719be2d6eb5aabb097f1808555
Jonas Forsberg <himynameisjonas@gmail.com>
2011-02-09 22:54:39 +0100

    added cool slider-navigation

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -8,4 +8,43 @@ $(function(){
       itemSelector : 'article'
     });
   });
+  
+  $('a.nav-link').live('click', function() {
+    history.pushState({ path: this.path }, '', this.href)
+    var direction = $(this).attr('id')
+    $.get(this.href, function(data) {
+      var old_photo = $("#main>article")
+      var new_photo = $(data).find("#main>article")
+      var window_width = $(window).width()
+      var left_offset = 0 - window_width
+
+      var new_nav = $(data).find("#nav")
+      if (direction == "prev") {
+          new_photo.css({
+              left: left_offset
+          })
+          old_photo.animate({left: window_width}, 'slow', function(){
+              old_photo.before(new_photo)
+              old_photo.remove()
+              new_photo.animate({left: 0}, 'slow');
+          });
+      } else {
+          new_photo.css({
+              left: window_width
+          })
+          old_photo.animate({left: left_offset}, 'slow', function(){
+              old_photo.before(new_photo)
+              old_photo.remove()
+              new_photo.animate({left: 0}, 'slow');
+          });
+      }
+      
+      $("#nav").replaceWith(new_nav)
+    })
+    return false
+  })
+  
+  $("#disqus-link").click(function(){
+      $("#disqus_thread").slideToggle("slow")
+  })
 });
